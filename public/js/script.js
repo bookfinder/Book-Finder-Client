ef886a2896143438e6cc50f5be19b39210852b5d
jack_dbernier <jdesjardinsbernier@gmail.com>
2011-11-12 12:53:40 -0500

    _

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,29 @@
 /* Author: 
 
-*/
+ */
 
-
-
-
-
-
-
+$('#form_search').submit(function(e) {
+  
+  var url = 'dummy-data.json';
+  
+  $.ajax({
+    url: url,
+    dataType: "json",
+    beforeSend: function( xhr ) {
+      console.log("beforeSend");
+    },
+    
+    success: function(data, status){
+      console.dir(data);
+    },
+    error: function(XHR, textStatus, errorThrown){
+      console.log(textStatus);
+      console.log(errorThrown);
+    }
+  });
+  
+  return false;
+});
 
 
 
